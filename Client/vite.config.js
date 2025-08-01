45cd92bd3f712f0c33893ac556bc226a14925101
robra0 <ararsarobra@gamil.com>
2025-08-02 00:09:41 +0300

    removed define from vite.config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import dotenv from 'dotenv'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
       tailwindcss(),
   ],
-  define:{
-    'process.env.VITE_API_URL':JSON.stringify(process.env.VITE_API_URL)
-  }
 })
