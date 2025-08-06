ec62f773a3af04f8b05f560426ae93b4ca979b22
robra0 <ararsarobra@gamil.com>
2025-08-06 22:31:17 +0300

    updated course

--- Server/app.js
+++ Server/app.js
@@ -131,12 +131,12 @@ app.get(`/api/c`,async (req,res)=>{
      const co = courseAssignments.find(co => co.course_id === c.course_id);
 
       return {
-           title:co?.assessments.title || "No title",
-          type:co?.assessments.type || 'not assigned',
+          title: co?.assessments?.[0]?.title || "No title",
+          type:co?.assessments?.[0]?.type || 'not assigned',
          instructor:c.instructor,
          course_name: c.course_name,
-          weight:co?.assessments.weight || "No Mark",
-          due:co.assessments.due_date || " Not known"
+          weight:co?.assessments?.[0]?.weight || "No Mark",
+          due:co?.assessments?.[0]?.due_date || " Not known"
  
       }
 
